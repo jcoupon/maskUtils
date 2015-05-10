7f15a424803611fdbd0d391deaf7bf6dd9b3f1ac
jcoupon <jean.coupon@gmail.com>
2015-05-10 17:59:47 +0200

    updated drawRandoms.py

--- python/hsc/maskUtils/drawRandoms.py
+++ python/hsc/maskUtils/drawRandoms.py
@@ -22,15 +22,13 @@
 #
 
 import numpy
-import pyfits
 
-
-import lsst.pex.config as pexConfig
-import lsst.pipe.base  as pipeBase
+import lsst.pex.config   as pexConfig
+import lsst.pipe.base    as pipeBase
 import matplotlib.pyplot as pyplot
-import lsst.afw.image      as afwImage
-from lsst.afw.geom import Box2D
-
+import lsst.afw.image    as afwImage
+from   lsst.afw.geom     import Box2D
+import lsst.afw.table    as afwTable
 
 from lsst.pipe.tasks.processCoadd import ProcessCoaddTask
 from lsst.pipe.tasks.coaddBase import ExistingCoaddDataIdContainer, getSkyInfo
@@ -39,7 +37,8 @@ from lsst.pipe.tasks.setPrimaryFlags import SetPrimaryFlagsTask
 
 #class DrawRandomsConfig(pexConfig.Config):
 class DrawRandomsConfig(ProcessCoaddTask.ConfigClass):
-    N = pexConfig.Field("Number of random points per patch", int, 100000)
+    N           = pexConfig.Field("Number of random points per patch", int, 100000)
+    fileOutName = pexConfig.Field("Name of output file", str, "ran.fits")
     
 class DrawRandomsTask(ProcessCoaddTask):
     _DefaultName = "drawRandoms"
@@ -47,72 +46,94 @@ class DrawRandomsTask(ProcessCoaddTask):
 
     def __init__(self, **kwargs):
         ProcessCoaddTask.__init__(self, **kwargs)
-  
+
+    def check_bit(self, bitmask, bit):
+        return ((bitmask&(1<<bit))!=0)
+
 
     def drawOnePoint(self, mask_array, dim, wcs, xy0, skyInfo):
         
-
         x = numpy.random.random()*(dim[0]-1)
         y = numpy.random.random()*(dim[0]-1)
 
-        value = mask_array[y, x]
+        bitmask = mask_array[y, x]
 
         radec = wcs.pixelToSky(x + xy0.getX(), y + xy0.getY())
         xy    = wcs.skyToPixel(radec)
 
         # inner patch
         innerFloatBBox = Box2D(skyInfo.patchInfo.getInnerBBox())
-        isPatchInner = innerFloatBBox.contains(xy)
+        isPatchInner   = innerFloatBBox.contains(xy)
 
         # inner tract
         sourceInnerTractId = skyInfo.skyMap.findTract(radec).getId()       
-        isTractInner = sourceInnerTractId == skyInfo.tractInfo.getId()
+        isTractInner       = sourceInnerTractId == skyInfo.tractInfo.getId()
 
+        # ra and dec in decimal degrees
         ra  = radec.getLongitude().asDegrees()  
         dec = radec.getLatitude ().asDegrees()
         
-        return ra, dec, value, isPatchInner, isTractInner
+        return ra, dec, bitmask, isPatchInner, isTractInner
 
 
     def run(self, dataRef):
 
+        # verbose
         self.log.info("Processing %s" % (dataRef.dataId))
 
-        # initialize
+        # get coadd and coadd info 
+        coadd   = dataRef.get(self.config.coaddName + "Coadd")
         skyInfo = getSkyInfo(coaddName=self.config.coaddName, patchRef=dataRef)
-        coadd = dataRef.get(self.config.coaddName + "Coadd")
 
+        # wcs and reference point (wrt tract)
         wcs = coadd.getWcs()
-        xy0 = coadd.getXY0()   
+        xy0 = coadd.getXY0()  
 
+        # extract mask and convert into array
         mask = coadd.getMaskedImage().getMask()
-        dim = mask.getDimensions()
+        dim  = mask.getDimensions()
         mask_array = mask.getArray()
 
+        # get mask labels and bit values
+        mask_labels = mask.getMaskPlaneDict()
+        keys        = mask_labels.keys()
+        values      = mask_labels.values()
 
-        ra           = [0.0]*self.config.N
-        dec          = [0.0]*self.config.N
-        value        = [0]*self.config.N
-        isPatchInner = [True]*self.config.N
-        isTractInner = [True]*self.config.N
+        # create table that will store random points and mask values
+        schema = afwTable.Schema()
 
+        # define table fields
+        fields = [schema.addField("ra", type="F", doc="ra")]
+        fields.append(schema.addField("dec", type="F", doc="dec"))
+        fields.append(schema.addField("isPatchInner", type="I", doc="True if inside patch inner area"))
+        fields.append(schema.addField("isTractInner", type="I", doc="True if inside tract inner area"))
+        for key in keys:
+            fields.append(schema.addField(key, type="I", doc=key))
 
+        # create table object
+        table = afwTable.BaseCatalog(schema)
+
+        # loop over N random points
         for i in range(self.config.N):
-            ra[i], dec[i], value[i], isPatchInner[i], isTractInner[i] = self.drawOnePoint(mask_array, dim, wcs, xy0, skyInfo)
+            ra, dec, bitmask, isPatchInner, isTractInner = self.drawOnePoint(mask_array, dim, wcs, xy0, skyInfo)
+
+            record = table.addNew()
+            record.set(fields[0], ra)
+            record.set(fields[1], dec)
+            record.set(fields[2], isPatchInner)
+            record.set(fields[3], isTractInner)
+
+            for j in range(len(values)):
+                flag = self.check_bit(bitmask, values[j])
+                if flag:
+                    toto = 1
+                else:
+                    toto = 0
+                record.set(fields[4+j], toto)
 
 
-
-        c1 = pyfits.Column(name='ra',           format='D',  array=ra)
-        c2 = pyfits.Column(name='dec',          format='D',  array=dec)
-        c3 = pyfits.Column(name='value',        format='J',  array=value)
-        c4 = pyfits.Column(name='isPatchInner', format='L',  array=isPatchInner)
-        c5 = pyfits.Column(name='isTractInner', format='L',  array=isTractInner)
-        
-        tbhdu = pyfits.BinTableHDU.from_columns([c1, c2, c3, c4, c5])
-
-
-        tbhdu.writeto('ran.fits', clobber=True)
-
+        table.writeFits(self.config.fileOutName)
+      
         return
 
 
