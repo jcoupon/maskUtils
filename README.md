f282a3d36d7f95ee996f390fab82f97bf1c59ad7
Jean Coupon <jean.coupon@gmail.com>
2016-03-31 18:29:53 +0200

    changed default outputDir (now in ROOT)

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Creates a random catalogue for a given patch:
 
 `N`: controls the number of random points per patch (default is 100000). IMPORTANT: Supersedes Nden if set. Use with caution, patches (even within a single tract) may not have the same size, so that using N=fixed would result in non-constant random point density.
 
-`dirOutName`: Name of output directory (will write output files as dirOutName/FILTER/TRACT/PATCH/ran-FILTER-TRACT-PATCH.fits (default is current directory).
+`dirOutName`: Name of output directory (will write output files as dirOutName/FILTER/TRACT/PATCH/ran-FILTER-TRACT-PATCH.fits (default is ROOT/deepCoadd-results/).
 
 `fileOutName`: the name (with full path) of the output fits file (supersedes `dirOutName`).
 
--- python/hsc/maskUtils/drawRandoms.py
+++ python/hsc/maskUtils/drawRandoms.py
@@ -38,6 +38,8 @@ import lsst.afw.detection as afwDetection
 from lsst.pipe.tasks.coaddBase import CoaddBaseTask
 from lsst.pipe.tasks.setPrimaryFlags import SetPrimaryFlagsTask
 
+# from lsst.obs.hsc import HscMapper
+
 __all__ = ["DrawRandomsTask"]
 
 class DrawRandomsConfig(CoaddBaseTask.ConfigClass):
@@ -46,7 +48,7 @@ class DrawRandomsConfig(CoaddBaseTask.ConfigClass):
     Nden        = pexConfig.Field("Random number density per sq arcmin", float, 100)
     clobber     = pexConfig.Field("To overwrite existing file [default: True]", bool, True)
     #fileOutName = pexConfig.Field("Name of output file", str, "ran.fits")
-    dirOutName  = pexConfig.Field("Name of output directory (will write output files as dirOutName/FILTER/TRACT/PATCH/ran-FILTER-TRACT-PATCH.fits)", str, ".")
+    dirOutName  = pexConfig.Field("Name of output directory (will write output files as dirOutName/FILTER/TRACT/PATCH/ran-FILTER-TRACT-PATCH.fits)", str, "")
     fileOutName = pexConfig.Field("Name of output file (supersedes dirOutName)", str, "")
     test        = pexConfig.Field("To write a test table", bool, False)
     seed        = pexConfig.Field("Seed for random generator (default: based on patch id)", int, -1)
@@ -82,8 +84,6 @@ class DrawRandomsTask(CoaddBaseTask):
         expId = long(dataRef.get(self.config.coaddName + datasetName))
 
         return afwTable.IdFactory.makeSource(expId, 64 - expBits)
-
-
 
     def run(self, dataRef, selectDataList=[]):
         """
@@ -169,8 +169,6 @@ class DrawRandomsTask(CoaddBaseTask):
         # to get 5-sigma limiting magnitudes:
         # print -2.5*numpy.log10(5.0*sky_std/coadd.getCalib().getFluxMag0()[0])
 
-
-
         ms      = measureSourcesConfig.makeMeasureSources(self.schema)
         table  = afwTable.SourceTable.make(self.schema, self.makeIdFactory(dataRef))
         catalog = afwTable.SourceCatalog(table)
@@ -239,10 +237,21 @@ class DrawRandomsTask(CoaddBaseTask):
 
         # write catalog
         if self.config.fileOutName == "":
-            fileOutName = "{0}/{1}/{2}/{3}/ran-{1}-{2}-{3}.fits".format(self.config.dirOutName,dataRef.dataId["filter"],dataRef.dataId["tract"],dataRef.dataId["patch"])
-            self.mkdir_p(os.path.dirname(fileOutName))
+
+            # get output dir
+            # TO DO: create new PAF
+            # see /Users/coupon/local/source/hscPipe/install/DarwinX86/solvetansip/6.5.1p_hsc/python/hsc/meas/tansip/utils.py
+            # and /Users/coupon/local/source/hscPipe/install/DarwinX86/pex_policy/HSC-4.0.0/tests/Policy_1.py
+            if self.config.dirOutName == "":
+                dirOutName = dataRef.getButler().mapper.root+"/"+self.config.coaddName+"Coadd-results"
+                self.log.info("WARNING: the output file will be written in {0:s}.".format(dirOutName))
+            else:
+                dirOutName = self.config.dirOutName
+            fileOutName = "{0}/{1}/{2}/{3}/ran-{1}-{2}-{3}.fits".format(dirOutName,dataRef.dataId["filter"],dataRef.dataId["tract"],dataRef.dataId["patch"])
         else:
             fileOutName = self.config.fileOutName
+
+        self.mkdir_p(os.path.dirname(fileOutName))
         catalog.writeFits(fileOutName)
 
         # to do. Define output name in init (not in paf) and
@@ -278,202 +287,12 @@ class DrawRandomsTask(CoaddBaseTask):
         """
         return int(round(x) - .5) + (x > 0)
 
-
-    """
-    *******************************************************************
-    *******************************************************************
-                        BELOW IS NOT USED ANYMORE
-    *******************************************************************
-    *******************************************************************
-    """
-
-
-    def run_Old(self, dataRef, selectDataList=[]):
-
-        import pyfits as fits
-
-        if self.config.test:
-            self.testTable(read=True)
-            return
-
-        # verbose
-        self.log.info("Processing %s" % (dataRef.dataId))
-
-        # get coadd and coadd info
-        coadd   = dataRef.get(self.config.coaddName + "Coadd")
-
-        #coadd.setPsf(None)
-        #print coadd.getPsf()
-
-        skyInfo = self.getSkyInfo(dataRef)
-        #skyInfo = self.getSkyInfo(coaddName=self.config.coaddName, patchRef=dataRef)
-
-        # wcs and reference point (wrt tract)
-        wcs = coadd.getWcs()
-        xy0 = coadd.getXY0()
-
-        # extract mask and convert into array
-        mask = coadd.getMaskedImage().getMask()
-        dim  = mask.getDimensions()
-        mask_array = mask.getArray()
-
-        # Compute the area in degree
-        pixel_area =  coadd.getWcs().pixelScale().asDegrees()**2
-        area       = pixel_area * dim[0] * dim[1]
-
-        # print "total area = ", area
-
-        if self.config.N  == -1:
-            N = self.iround(area*self.config.Nden*60.0*60.0)
-        else:
-            N = self.config.N
-
-        # get mask labels and bit values
-        mask_labels = mask.getMaskPlaneDict()
-        keys        = mask_labels.keys()
-        values      = mask_labels.values()
-
-        # initialize arrays
-        ra           = [0.0]*N
-        dec          = [0.0]*N
-        isPatchInner = [True]*N
-        isTractInner = [True]*N
-        flags        = [[True]*N for i in range(len(keys))]
-
-        # loop over N random points
-        for i in range(N):
-            ra[i], dec[i], bitmask, isPatchInner[i], isTractInner[i] = self.drawOnePoint(mask_array, dim, wcs, xy0, skyInfo)
-
-            for j, value in enumerate(values):
-                flags[j][i] = self.check_bit(bitmask, value)
-
-        # column definition
-        cols = fits.ColDefs([fits.Column(name="ra",           format="E", array=ra)])
-        cols.add_col(        fits.Column(name="dec",          format="E", array=dec))
-        cols.add_col(        fits.Column(name="isPatchInner", format="L", array=isPatchInner))
-        cols.add_col(        fits.Column(name="isTractInner", format="L", array=isTractInner))
-        for j, key in enumerate(keys):
-            cols.add_col(        fits.Column(name=key,            format="L", array=flags[j]))
-
-        # create table object - method depends on pyfits version
-        pyfits_v = [self.int_robust(v) for v in (fits.__version__).split('.')]
-        if (pyfits_v[0] > 3) | (pyfits_v[0] == 3 & pyfits_v[1] >= 3):
-            tbhdu = fits.BinTableHDU.from_columns(cols)
-        else:
-            tbhdu = fits.new_table(cols)
-
-        # add info in header
-        header = fits.Header()
-        header['AREA'] = (area, "in sq. degrees")
-
-        # write table
-        hdulist = fits.HDUList([fits.PrimaryHDU(header=header), tbhdu])
-
-        #tbhdu.writeto(self.config.fileOutName, clobber=self.config.clobber)
-        hdulist.writeto(self.config.fileOutName, clobber=self.config.clobber)
-
-        return
-
-    def check_bit(self, bitmask, bit):
-        return ((bitmask&(1<<bit))!=0)
-
-
-    def drawOnePointOLD(self, mask_array, dim, wcs, xy0, skyInfo):
-
-        x = numpy.random.random()*(dim[0]-1)
-        y = numpy.random.random()*(dim[1]-1)
-
-        bitmask = mask_array[y, x]
-
-        radec = wcs.pixelToSky(x + xy0.getX(), y + xy0.getY())
-        xy    = wcs.skyToPixel(radec)
-
-
-        #self.exp.getWcs().pixelToSky(afwGeom.Point2D(self.x, self.y))
-
-        # inner patch
-        innerFloatBBox = Box2D(skyInfo.patchInfo.getInnerBBox())
-        isPatchInner   = innerFloatBBox.contains(xy)
-
-        # inner tract
-        sourceInnerTractId = skyInfo.skyMap.findTract(radec).getId()
-        isTractInner       = sourceInnerTractId == skyInfo.tractInfo.getId()
-
-        # ra and dec in decimal degrees
-        ra  = radec.getLongitude().asDegrees()
-        dec = radec.getLatitude ().asDegrees()
-
-        return ra, dec, bitmask, isPatchInner, isTractInner
-
-
-    def testTable(self, read=False):
-
-        #import lsst.afw.geom as afwGeom
-
-
-        #print afwGeom.Point2D(100.23, 100)
-
-
-        #exit(-1)
-
-        # to do
-        # this table cannot be read by Topcat and I have no clue why
-
-
-#        schema = afwTable.SourceTable.makeMinimalSchema()
-#        task = measAlg.SourceMeasurementTask(schema, config=config)
-#        source = catalog.addNew()
-#        source.set("id", 12345)
-
-
-
-        # create table
-#        schema = afwTable.Schema()
-        schema = afwTable.SourceTable.makeMinimalSchema()
-
-        # define table fields
-        fields = [schema.addField("ra", type="F", doc="ra")]
-        fields.append(schema.addField("dec", type="F", doc="dec"))
-        fields.append(schema.addField("isPatchInner", type="Flag", doc="True if inside patch inner area"))
-        fields.append(schema.addField("isTractInner", type="Flag", doc="True if inside tract inner area"))
-
-        # create table object
-#        catalog = afwTable.BaseCatalog(schema)
-        catalog = afwTable.SourceCatalog(schema)
-
-
-        # fill the table
-        record = catalog.addNew()
-#        help(record)
-
-
-        record.set(fields[0], 1.0)
-        record.set(fields[1], 2.0)
-        for i in range(2, 4):
-            record.set(fields[i], True)
-
-        for source in record:
-            print source.get("isPatchInner")
-
-        # save the table
-        catalog.writeFits("test.fits")
-
-        if read:
-
-            #FILE = fits.open("/Users/coupon/data/HSC/SSP/rerun/CLAUDS/deepCoadd-results/HSC-Y/1/5,5/src-HSC-Y-1-5,5.fits")
-            FILE = fits.open("test.fits")
-            d    = FILE[1].data
-
-            print FILE.info()
-            print FILE[0].header
-
-            FILE.close()
-
-        return
-
-
-    def int_robust(self, s):
-        try:
-            return int(s)
-        except ValueError:
-            return 0
+#    def check_bit(self, bitmask, bit):
+#        return ((bitmask&(1<<bit))!=0)
+
+
+#    def int_robust(self, s):
+#        try:
+#            return int(s)
+#        except ValueError:
+#            return 0
